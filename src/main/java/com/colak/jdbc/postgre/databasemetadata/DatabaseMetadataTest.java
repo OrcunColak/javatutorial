42787288fc4d1eb4c51a759549d2b6f9eb802b16
Orçun Çolak <orcun.colak@hazelcast.com>
2024-01-24 10:38:54 +0300

    DatabaseMetadata for postgres

--- src/main/java/com/colak/jdbc/postgre/databasemetadata/DatabaseMetadataTest.java
+++ src/main/java/com/colak/jdbc/postgre/databasemetadata/DatabaseMetadataTest.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Slf4j
 public class DatabaseMetadataTest {
@@ -19,10 +20,75 @@ public class DatabaseMetadataTest {
             DatabaseMetaData metaData = connection.getMetaData();
 
             // Specify the table name for which you want to retrieve metadata
-            String tableName = "all_char_types_table";
 
-            // Get columns metadata
-            ResultSet resultSet = metaData.getColumns(null, null, tableName, null);
+            String tableName = "all_char_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "all_numeric_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "datetime_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "binary_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "boolean_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "person_mood";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "bit_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "uuid_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "inet_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "cidr_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "json_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "xml_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "geometric_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "array_types_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+            tableName = "ranges_table";
+            log.info("---------- {} ----------", tableName);
+            printTable(metaData, tableName);
+
+        } catch (Exception exception) {
+            log.error("Exception ", exception);
+        }
+    }
+
+    private static void printTable(DatabaseMetaData metaData, String tableName) throws SQLException {
+        // Get columns metadata
+        try (ResultSet resultSet = metaData.getColumns(null, null, tableName, null)) {
             while (resultSet.next()) {
                 String columnName = resultSet.getString("COLUMN_NAME");
                 String dataType = resultSet.getString("TYPE_NAME");
@@ -35,11 +101,6 @@ public class DatabaseMetadataTest {
                 log.info("Decimal Digits: {}", scale);
                 log.info("----------");
             }
-
-            // You can also get other metadata information as needed
-
-        } catch (Exception exception) {
-            log.error("Exception ", exception);
         }
     }
 }
