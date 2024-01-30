be429a1ba41279d314f51d64b381fb083c035519
Orçun Çolak <orcun.colak@hazelcast.com>
2024-01-30 12:08:49 +0300

    statement batch insert example

--- src/main/java/com/colak/jdbc/statement/StBatchInsertTest.java
+++ src/main/java/com/colak/jdbc/statement/StBatchInsertTest.java
@@ -1,44 +1,50 @@
-package com.colak.jdbc.preparedstatement.batch;
+package com.colak.jdbc.statement;
 
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
 
 @Slf4j
-public class BatchInsertTest {
+public class StBatchInsertTest {
 
     public static void main(String[] args) throws SQLException {
         String jdbcUrl = "jdbc:postgresql://localhost:5432/db";
         String username = "postgres";
         String password = "postgres";
-        String insertEmployeeSQL = "INSERT INTO PSEMPLOYEE(ID, NAME) VALUES (?,?)";
 
         try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-             PreparedStatement preparedStatement = connection.prepareStatement(insertEmployeeSQL)) {
+             Statement statement = connection.createStatement()) {
 
+            dropTable(connection);
             createTable(connection);
 
             for (int index = 0; index < 100; index++) {
-                preparedStatement.setInt(1, index);
-                preparedStatement.setString(2, String.valueOf(index));
-                preparedStatement.addBatch();
-                preparedStatement.clearParameters();
+                String sql = String.format("INSERT INTO STEMPLOYEE(ID, NAME) VALUES(%d, '%s')", index, index);
+                statement.addBatch(sql);
+
             }
-            int[] ints = preparedStatement.executeBatch();
+            int[] ints = statement.executeBatch();
             boolean allMatch = Arrays.stream(ints).allMatch(i -> i == 1);
             log.info("executeBatch result : {}", allMatch);
 
         }
     }
 
+    private static void dropTable(Connection connection) throws SQLException {
+        String sql = "DROP TABLE STEMPLOYEE";
+        try (Statement statement = connection.createStatement()) {
+            boolean result = statement.execute(sql);
+            log.info("createTable result : {}", result);
+        }
+    }
+
     private static void createTable(Connection connection) throws SQLException {
         String sql = """
-                CREATE TABLE IF NOT EXISTS PSEMPLOYEE (
+                CREATE TABLE IF NOT EXISTS STEMPLOYEE (
                     ID SERIAL PRIMARY KEY,
                     NAME VARCHAR(255)
                 );
