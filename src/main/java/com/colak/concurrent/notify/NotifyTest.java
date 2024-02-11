141ad640d1eeae0d7566b068e8fe6d4f2b5a9210
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-11 22:14:48 +0300

    notify example

---
+++ src/main/java/com/colak/concurrent/notify/NotifyTest.java
@@ -0,0 +1,41 @@
+package com.colak.concurrent.notify;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * See <a href="https://medium.com/@reetesh043/understanding-the-difference-between-wait-and-sleep-methods-64958feffbb8">...</a>
+ */
+@Slf4j
+public class NotifyTest {
+
+    private static class MyWaitingThread extends Thread {
+        @Override
+        public void run() {
+            synchronized (this) {
+                log.info("Thread 1 is waiting...");
+                try {
+                    this.wait(); // Thread 1 waits until notified by another thread
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                log.info("Thread 1 has been notified.");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyWaitingThread thread1 = new MyWaitingThread();
+        thread1.start();
+
+        // Sleep for 1 second
+        TimeUnit.SECONDS.sleep(1);
+
+        synchronized (thread1) {
+            // Notify thread1 to resume
+            thread1.notify();
+        }
+        thread1.join();
+    }
+}
