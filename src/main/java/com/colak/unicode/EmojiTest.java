4c0d222c9dc89e4b57416e3b629811c99fbad586
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-29 08:15:24 +0300

    Emoji example

---
+++ src/main/java/com/colak/unicode/EmojiTest.java
@@ -0,0 +1,15 @@
+package com.colak.unicode;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EmojiTest {
+
+    public static void main(String[] args) {
+
+        log.info("\uD83D\uDC8B"); // kiss
+        log.info("\uD83D\uDC98"); // heart
+        log.info("\uD83D\uDE0E"); // smiling face with sunglasses
+
+    }
+}
