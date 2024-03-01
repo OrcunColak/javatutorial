7dbd4ab7e04db4c4d8a263c1e6ac84958f91950e
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-01 09:16:01 +0300

    thread examples

---
+++ src/main/java/com/colak/concurrent/threadlocal/ThreadLocalTest.java
@@ -0,0 +1,14 @@
+package com.colak.concurrent.threadlocal;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ThreadLocalTest {
+
+    public static void main(String[] args) {
+        ThreadLocal<Integer> threadLocal = ThreadLocal.withInitial(() -> 0);
+        threadLocal.set(42); // Set a thread-local variable
+        int value = threadLocal.get();
+        log.info("Value : {}", value);
+    }
+}
