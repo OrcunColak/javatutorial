255d29829d16e0abc8dc0c95daf05a52cd38b68a
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-05 10:38:49 +0300

    generics examples

---
+++ src/main/java/com/colak/generics/ContravariantCopyTest.java
@@ -0,0 +1,25 @@
+package com.colak.generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ContravariantCopyTest {
+
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(1,2,3,4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+
+        List<Object> myObjs = new ArrayList<>();
+        copy(myInts, myObjs);
+        copy(myDoubles, myObjs);
+    }
+
+    // Covariance: accept subtypes : Covariant types are read-only,
+    // Contravariance: accept supertypes : Contravariant types are write-only.
+    private static void copy(List<? extends Number> covariantSource, List<? super Number> contraVariantDestiny) {
+        for(Number number : covariantSource) {
+            contraVariantDestiny.add(number);
+        }
+    }
+}
