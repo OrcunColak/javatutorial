b415ee5891a27fd3c4f6da3a42e64daf7b6148d8
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-12 15:59:37 +0300

    lock examples

--- src/main/java/com/colak/concurrent/reentrantlock/ReentrantLockTest.java
+++ src/main/java/com/colak/concurrent/reentrantlock/ReentrantLockTest.java
@@ -4,28 +4,31 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * At the end of this test the balance must be 0 because wife is withdrawing more than deposit
+ */
 @Slf4j
-class ReentratnLockTest {
+class ReentrantLockTest {
 
     private final ReentrantLock lock = new ReentrantLock();
     private int balance = 100;
 
     public static void main(String[] args) throws InterruptedException {
-        ReentratnLockTest readWriteLockTest = new ReentratnLockTest();
+        ReentrantLockTest readWriteLockTest = new ReentrantLockTest();
         readWriteLockTest.testLock();
     }
 
     private void testLock() throws InterruptedException {
         Thread wifeThread = new Thread(() -> {
             for (int index = 0; index < 100; index++) {
-                log.info("Withdraw : {}", withdraw(2));
+                log.info("Withdraw balance : {}", withdraw(2));
             }
         });
         wifeThread.start();
 
         Thread husbandThread = new Thread(() -> {
             for (int index = 0; index < 100; index++) {
-                log.info("Deposit : {}", deposit(1));
+                log.info("Deposit balance : {}", deposit(1));
             }
         });
         husbandThread.start();
@@ -35,31 +38,25 @@ class ReentratnLockTest {
     }
 
     private int withdraw(int value) {
-        // Acquiring the read lock
         lock.lock();
         try {
             if (balance >= value) {
-                // Perform read operations on the shared resource
                 balance = balance - value;
                 return balance;
             } else {
                 return balance;
             }
         } finally {
-            // read lock released
             lock.unlock();
         }
     }
 
     public int deposit(int value) {
-        // acquiring the write lock
         lock.lock();
         try {
-            // Perform write operations on the shared resource
             balance += value;
             return balance;
         } finally {
-            // write lock released
             lock.unlock();
         }
     }
---
+++ src/main/java/com/colak/concurrent/reentrantlock/ReentrantLockTryLockTest.java
@@ -0,0 +1,64 @@
+package com.colak.concurrent.reentrantlock;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * At the end of this test the balance does not have to be 0 because wife may skip locking when husband is depositing
+ */
+@Slf4j
+class ReentrantLockTryLockTest {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private int balance = 100;
+
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLockTryLockTest readWriteLockTest = new ReentrantLockTryLockTest();
+        readWriteLockTest.testLock();
+    }
+
+    private void testLock() throws InterruptedException {
+        Thread wifeThread = new Thread(() -> {
+            for (int index = 0; index < 100; index++) {
+                log.info("Withdraw balance: {}", withdraw(2));
+            }
+        });
+        wifeThread.start();
+
+        Thread husbandThread = new Thread(() -> {
+            for (int index = 0; index < 100; index++) {
+                log.info("Deposit balance : {}", deposit(1));
+            }
+        });
+        husbandThread.start();
+
+        husbandThread.join();
+        wifeThread.join();
+    }
+
+    private int withdraw(int value) {
+        if (lock.tryLock()) {
+            try {
+                if (balance >= value) {
+                    balance = balance - value;
+                }
+            } finally {
+                // read lock released
+                lock.unlock();
+            }
+        }
+        return balance;
+    }
+
+    public int deposit(int value) {
+        if (lock.tryLock()) {
+            try {
+                balance += value;
+            } finally {
+                lock.unlock();
+            }
+        }
+        return balance;
+    }
+}
