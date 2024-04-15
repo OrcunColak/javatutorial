28d3bcee30541429bde7491005059e90ecb3da21
Orçun Çolak <orcun.colak@hazelcast.com>
2024-04-15 14:20:09 +0300

    javadoc example

---
+++ src/main/java/com/colak/javadoc/JavadocTest.java
@@ -0,0 +1,14 @@
+package com.colak.javadoc;
+
+/**
+ * @author John Doe
+ * @version 1.2.3
+ * @since 1.0
+ * <p>
+ * See <a href="https://medium.com/@tecnicorabi/mastering-javadocs-a-comprehensive-guide-2bd7b9d6d8dc">source link</a>
+ * <p>
+ * Command is javadoc -d doc -sourcepath src/main/java -subpackages com.colak.javadoc -exclude com.example.internal
+ */
+public class JavadocTest {
+
+}
