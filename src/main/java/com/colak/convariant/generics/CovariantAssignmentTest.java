a9eac316d7fff166981653bf36626ed25adf7b8b
Orçun Çolak <orcun.colak@hazelcast.com>
2024-05-08 09:29:28 +0300

    covariant examples

--- src/main/java/com/colak/convariant/generics/CovariantAssignmentTest.java
+++ src/main/java/com/colak/convariant/generics/CovariantAssignmentTest.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 // Generic Types are Invariant
-class ContravariantAssignmentTest {
+class CovariantAssignmentTest {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(100, 200);
