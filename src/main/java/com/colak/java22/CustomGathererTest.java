0cc778ffae2f4bf26cc9e49d4f869bd3e14992a9
Orçun Çolak <orcun.colak@hazelcast.com>
2024-06-21 13:58:43 +0300

    Gatherer example

---
+++ src/main/java/com/colak/java22/CustomGathererTest.java
@@ -0,0 +1,71 @@
+package com.colak.java22;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+import java.util.stream.Gatherer;
+
+
+// See https://levelup.gitconnected.com/java-22-explaining-new-features-with-examples-7fb2571188c8"
+@Slf4j
+class CustomGathererTest {
+
+    record LargestInt(int limit) implements Gatherer<Integer, List<Integer>, Integer> {
+        @Override
+        public Supplier<List<Integer>> initializer() {
+            return () -> new ArrayList<>(1);
+        }
+
+        @Override
+        public Integrator<List<Integer>, Integer, Integer> integrator() {
+            return Integrator.of((max, element, downstream) -> {
+                if (max.isEmpty()) max.addFirst(element);
+                else if (element > max.getFirst()) max.set(0, element);
+                if (element >= limit) {
+                    downstream.push(element);
+                    return false;
+                }
+                return true;
+            });
+        }
+
+        @Override
+        public BinaryOperator<List<Integer>> combiner() {
+            return (leftMax, rightMax) -> {
+                if (leftMax.isEmpty()) return rightMax;
+                if (rightMax.isEmpty()) return leftMax;
+                int leftVal = leftMax.getFirst();
+                int rightVal = rightMax.getFirst();
+                return (leftVal > rightVal) ? leftMax : rightMax;
+            };
+        }
+
+        @Override
+        public BiConsumer<List<Integer>, Downstream<? super Integer>> finisher() {
+            return (max, downstream) -> {
+                if (!max.isEmpty()) downstream.push(max.getFirst());
+            };
+        }
+    }
+
+    public static void main() {
+        fold();
+
+    }
+
+    // fold is a stateful many-to-one gatherer which constructs an aggregate incrementally and emits that aggregate when no more input elements exist.
+    private static void fold() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        List<Integer>  list = numbers.stream()
+                .gather(new LargestInt(10))
+                .toList();
+        // 5
+        log.info(STR."List \{list}");
+    }
+
+}
--- src/main/java/com/colak/java22/StreamGatherersTest.java
+++ src/main/java/com/colak/java22/StreamGatherersTest.java
@@ -5,16 +5,46 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 import java.util.stream.Gatherers;
 
-/**
- * See <a href="https://levelup.gitconnected.com/java-22-explaining-new-features-with-examples-7fb2571188c8">...</a>
- */
+
+
+// See https://levelup.gitconnected.com/java-22-explaining-new-features-with-examples-7fb2571188c8"
 @Slf4j
-public class StreamGatherersTest {
-    public static void main(String[] args) {
+class StreamGatherersTest {
+
+    public static void main() {
+        fold();
+        mapConcurrent();
         fixedSize();
         slidingWindow();
     }
 
+    // fold is a stateful many-to-one gatherer which constructs an aggregate incrementally and emits that aggregate when no more input elements exist.
+    private static void fold() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        Integer fold = numbers.stream()
+                .gather(Gatherers.fold(() -> 0, Integer::sum))
+                .findFirst()
+                .orElseThrow();
+        // 15
+        log.info(STR."Fold \{fold}");
+    }
+
+    // mapConcurrent is a stateful one-to-one gatherer which invokes a supplied function for each input element concurrently, up to a supplied limit.
+    private static void mapConcurrent() {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
+
+        List<Integer> mapConcurrent = numbers.stream()
+                .gather(Gatherers.mapConcurrent(4, a -> a * 2))
+                .toList();
+        //[2, 4, 6, 8, 10]
+        log.info(STR."MapConcurrent \{mapConcurrent}");
+    }
+
+
+    // windowSliding is a stateful many-to-many gatherer which groups input elements into lists of a supplied size.
+    // After the first window, each subsequent window is created from a copy of its predecessor by dropping the first
+    // element and appending the next element from the input stream.
     private static void slidingWindow() {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5);
 
@@ -22,9 +52,11 @@ public class StreamGatherersTest {
                 .gather(Gatherers.windowSliding(3))
                 .toList();
         // [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
-        log.info(STR."SlidingWindow\{slidingWindows}");
+        log.info(STR."SlidingWindow \{slidingWindows}");
     }
 
+    // windowFixed is a stateful many-to-many gatherer which groups input elements into lists of a supplied size,
+    // emitting the windows downstream when they are full.
     private static void fixedSize() {
         List<Integer> numbers = List.of(1, 2, 3, 4, 5);
         List<List<Integer>> fixedWindows = numbers.stream()
@@ -32,8 +64,6 @@ public class StreamGatherersTest {
                 .toList();
 
         // [[0, 1, 2], [3, 4, 5]]
-        log.info(STR."fixedWindow\{fixedWindows}");
+        log.info(STR."fixedWindow \{fixedWindows}");
     }
-
-
 }
