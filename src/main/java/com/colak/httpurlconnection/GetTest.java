01cd076271ef3c63d688debb5a7367b7e33a160d
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-10 11:09:54 +0300

    HttpURLConnection example

---
+++ src/main/java/com/colak/httpurlconnection/GetTest.java
@@ -0,0 +1,35 @@
+package com.colak.httpurlconnection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+@Slf4j
+public class GetTest {
+
+    public static void main() throws IOException {
+        URL url = URI.create("http://google.com").toURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            log.info(response.toString());
+        } else {
+            log.info("GET request failed");
+        }
+    }
+}
