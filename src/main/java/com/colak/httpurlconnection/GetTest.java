769fd4c7dc43fef03d5c18815f7cccec2ca9f163
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-17 15:16:32 +0300

    http connection example

--- src/main/java/com/colak/httpurlconnection/GetTest.java
+++ src/main/java/com/colak/httpurlconnection/GetTest.java
@@ -1,5 +1,6 @@
 package com.colak.httpurlconnection;
 
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
@@ -8,28 +9,37 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
+@UtilityClass
 public class GetTest {
 
     public static void main() throws IOException {
         URL url = URI.create("http://google.com").toURL();
+
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
+        connection.setConnectTimeout(2_000);
+        // connection.setReadTimeout(2_000);
+
+        // This call will connect to given URL
         int responseCode = connection.getResponseCode();
 
         if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
             StringBuilder response = new StringBuilder();
 
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+            try (InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8);
+                 BufferedReader in = new BufferedReader(inputStreamReader)) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
             }
-            in.close();
             log.info(response.toString());
         } else {
             log.info("GET request failed");
         }
+        connection.disconnect();
     }
 }
