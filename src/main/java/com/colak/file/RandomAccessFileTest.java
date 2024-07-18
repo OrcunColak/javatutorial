4c61ebfd39cb017d3b9f33c08910c9d7ccf134a0
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-18 11:46:13 +0300

    RandomAccessFile example

--- src/main/java/com/colak/file/RandomAccessFileTest.java
+++ src/main/java/com/colak/file/RandomAccessFileTest.java
@@ -14,7 +14,7 @@ public class RandomAccessFileTest {
 
     public static void main() throws IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        URL resource = classLoader.getResource("test.properties");
+        URL resource = classLoader.getResource("test.bat");
         assert resource != null;
         File file = new File(resource.getFile());
         try (RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r")) {
