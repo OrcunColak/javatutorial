4e58445811304d6a91b1e9742c8214c018fe0f30
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-19 11:04:32 +0300

    add comments

--- src/main/java/com/colak/java17/textblock/TextBlockTest.java
+++ src/main/java/com/colak/java17/textblock/TextBlockTest.java
@@ -1,11 +1,13 @@
 package com.colak.java17.textblock;
 
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@UtilityClass
 public class TextBlockTest {
 
-    public static void main(String[] args) {
+    public static void main() {
         newLineTest();
         escapeSequenceTest();
 
@@ -16,18 +18,21 @@ public class TextBlockTest {
                 line1 \
                 line2
                 """;
-        // -1
+        // line1
+        // line2
+        // \n
         log.info("str : {}", str);
     }
 
     // Text block is not platform dependent. It only adds \n to line ending
     private static void newLineTest() {
-        // line1 line2
+        // line1
+        // line2
         String str = """
                 line1
-                line2
-                """;
+                line2""";
         int indexOf = str.indexOf("\r\n");
+        // -1
         log.info("Index : {}", indexOf);
     }
 }
