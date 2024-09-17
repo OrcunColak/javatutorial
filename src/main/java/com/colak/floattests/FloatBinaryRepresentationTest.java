a08334559afdb037165ca54b28250471e337b2e0
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-17 08:10:20 +0300

    FloatBinaryRepresentation example

---
+++ src/main/java/com/colak/floattests/FloatBinaryRepresentationTest.java
@@ -0,0 +1,33 @@
+package com.colak.floattests;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UtilityClass
+class FloatBinaryRepresentationTest {
+
+    public static void main() {
+        float num = 12.375f;  // Example float number
+
+        // Convert the float to an integer bit representation using Float.floatToIntBits
+        int intBits = Float.floatToIntBits(num);
+
+        // Extract the sign (1 bit)
+        int sign = (intBits >> 31) & 1;
+
+        // Extract the exponent (8 bits)
+        int exponent = (intBits >> 23) & 0xFF;
+
+        // Extract the mantissa (23 bits)
+        int mantissa = intBits & 0x7FFFFF;
+
+        // Print the results
+        log.info("Float: {}", num);
+        String binaryString = String.format("%32s", Integer.toBinaryString(intBits)).replace(' ', '0');
+        log.info("Binary Representation: {}", binaryString);
+        log.info("Sign Bit: {}", sign);
+        log.info("Exponent: {} (Unbiased: {})", Integer.toBinaryString(exponent), (exponent - 127));
+        log.info("Mantissa: {}", Integer.toBinaryString(mantissa));
+    }
+}
