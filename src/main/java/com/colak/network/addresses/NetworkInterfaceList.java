370e011801b4ff1e1be7ba95daf31d776fbc9fe1
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-18 15:33:15 +0300

    refactor network addresses test

--- src/main/java/com/colak/network/addresses/NetworkInterfaceList.java
+++ src/main/java/com/colak/network/addresses/NetworkInterfaceList.java
@@ -1,21 +1,27 @@
-package com.colak.network.multicast;
+package com.colak.network.addresses;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
-public class NetworkInterfaceList {
+// List interfaces and their addresses
+@Slf4j
+@UtilityClass
+class NetworkInterfaceList {
 
     public static void main() throws SocketException {
         Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
         while (interfaces.hasMoreElements()) {
             NetworkInterface networkInterface = interfaces.nextElement();
-            System.out.println("Interface: " + networkInterface.getName());
+            log.info("Interface Name: {}", networkInterface.getName());
             Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
             while (addresses.hasMoreElements()) {
                 InetAddress address = addresses.nextElement();
-                System.out.println("  Address: " + address.getHostAddress());
+                log.info("  Address: {}", address.getHostAddress());
             }
         }
     }
